d9aa56835df637402f29f0651f4fcdd462e3a55c
Yann <yann@pcportable.home>
2016-04-10 21:04:00 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*  %{Cpp:License:FileName} %{Cpp:License:ClassName} - Yann BOUCHER (yann) 10/04/2016
+**
+**
+**            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+**                    Version 2, December 2004
+**
+** Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+**
+** Everyone is permitted to copy and distribute verbatim or modified
+** copies of this license document, and changing it is allowed as long
+** as the name is changed.
+**
+**            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+**   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+**
+**  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
